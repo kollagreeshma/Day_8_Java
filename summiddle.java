18c718fb404dced5c65674c7cac65550e7b6eacd
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-09 11:42:01 +0530

    Add files via upload

---
+++ summiddle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class summiddle{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ns[]=new int[n];
+        int s=0,k=0;
+        for(int i=1;i<=n;i++)
+        {
+            s+=i;
+            ns[k]=s;
+            k++;
+        }
+        System.out.println((ns[n/2]));
+    }
+}
