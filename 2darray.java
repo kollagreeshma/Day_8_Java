ca77b8a1ec8eb234a095be26edcdf304beb14e02
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-09 15:38:43 +0530

    Add files via upload

---
+++ 2darray.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class darray{
+    public static void main(String args[]){
+        int a[][]={{1,2},{3,4}};
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
